726fedcd25b5a24c0b48dff0fb8825dba411c814
07RRJ <rasmusrudko07@gmail.com>
2025-06-10 01:51:38 +0200

    1.6

--- main.js
+++ main.js
@@ -1,17 +1,9 @@
 "use strict";
-const btnHamburger = document.getElementById("hamburger-button");
-const primaryNavigation = document.getElementById("primary-navigation");
-const sidebar = document.getElementById("sidebar");
 
-document.querySelector('#hamburger-button').addEventListener('click', function () {
-    document.querySelector('#sidebar').classList.toggle('open');
-});
+const primaryNavigation = document.getElementById("primary-navigation");
+const btnHamburger = document.getElementById("hamburger-button");
 
 btnHamburger.addEventListener("click", () => {
-
     primaryNavigation.classList.toggle("open");
-    sidebar.classList.toggle("open");
-
-    const expanded = btnHamburger.getAttribute("aria-expanded") === "true";
-    btnHamburger.setAttribute("aria-expanded", String(!expanded));
-});
+    btnHamburger.setAttribute("aria-expanded", btnHamburger.getAttribute("aria-expanded") === "false" ? "true" : "false");
+})
